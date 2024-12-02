7781a55c1f577b4783d489c235c3ba0003b58ad2
Mario <falkifn03177@gmail.com>
2024-12-02 01:38:05 +0100

    02-12-2024.1:37

---
+++ Paginas/Cruceros24/readme.txt
@@ -0,0 +1,1 @@
+Hacer la pagina de ofertas con un menu a la izquierda con filtro para buscar
--- Paginas/Samis/script.js
+++ Paginas/Samis/script.js
@@ -2,3 +2,18 @@ function toggleSection(id) {
     const section = document.getElementById(id);
     section.classList.toggle("visible");
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.getElementById("menu-btn");
+    const menu = document.getElementById("menu");
+
+    menuBtn.addEventListener("click", () => {
+        // Cambiar entre mostrar y ocultar el menú
+        if (menu.style.display === "block") {
+            menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+    });
+});
